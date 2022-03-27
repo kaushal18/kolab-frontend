0dd461e74ece8d624987a533128010dcf8d16891
kaushal yadav <kaushal.y1801@gmail.com>
2022-03-27 21:50:47 +0530

    Added constants

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-// import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 /**
  * This component checks if token is password protected and redirectes it to appropriate component
  */
 const Login = () => {
-  // const { token } = useParams();
+  const { token } = useParams<{ token: string }>();
 
 
   // return <h1>Login</h1>;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { io, Socket } from "socket.io-client";
 import ContentArea from "./ContentArea/ContentArea";
 import useDebounce from "../hooks/useDebounce";
-const ENDPOINT = "http://localhost:5000";
+const BACKEND_ENDPOINT = "http://localhost:5000";
 
 const Main = () => {
   const { token } = useParams<{ token: string }>();
@@ -11,7 +11,7 @@ const Main = () => {
   const [socket, setSocket] = useState<Socket>();
 
   useEffect(() => {
-    const socket = io(ENDPOINT, { query: { token: token } });
+    const socket = io(BACKEND_ENDPOINT, { query: { token: token } });
     socket.on("connect", () => {
       console.log("connected", socket.id);
     });
@@ -23,7 +23,7 @@ const Main = () => {
 
   // listen for incomming messages
   useEffect(() => {
-    socket?.on("message", (message) => {
+    socket?.on("document", (message) => {
       console.log("incomming", message);
       // TODO - resolve conflicts
       setLocalDocument(message);
@@ -32,7 +32,7 @@ const Main = () => {
 
   const debounceAndEmit = useDebounce((newLocalDocument, socket) => {
     console.log(newLocalDocument);
-    socket?.emit("message", newLocalDocument);
+    socket?.emit("document", newLocalDocument);
   }, 500);
 
   // emit current changes when localDocument modifies
